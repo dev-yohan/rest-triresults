bf1519867be292fe7ea594f181ad6e483bd56628
dev-yohan <yohanmorales@hotmail.com>
2016-02-05 17:56:06 -0500

    custom_types_spec rq03

---
+++ app/models/racer_info.rb
@@ -0,0 +1,14 @@
+class RacerInfo
+  include Mongoid::Document
+
+  field :racer_id, as: :_id
+  field :_id, default:->{ racer_id }
+  field :fn,  type: String, as: :first_name
+  field :ln,  type: String, as: :last_name
+  field :g,   type: String, as: :gender
+  field :yr,  type: Integer, as: :birth_year
+  field :res, type: Address, as: :residence
+
+
+
+end  
