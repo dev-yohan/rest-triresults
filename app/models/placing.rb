b3bfae603aaf61d6898dc9a63ccd26f9a450137e
dev-yohan <yohanmorales@hotmail.com>
2016-02-05 17:42:37 -0500

    custom_types_spec rq03

---
+++ app/models/placing.rb
@@ -0,0 +1,42 @@
+class Placing
+
+  attr_accessor :name, :place
+
+  def initialize name, place
+    @name  = name
+    @place = place
+  end  
+
+  def mongoize
+    {:name => @name, :place => @place}
+  end
+
+  def self.mongoize(object) 
+    case object
+      when Placing then object.mongoize
+      when Hash then 
+        Placing.new(object[:name], object[:place]).mongoize
+      else object
+    end
+  end
+
+  def self.demongoize(object)
+    case object
+      when nil then
+        nil
+      when Hash then
+        Placing.new(object[:name], object[:place])
+      when Placing then
+        object
+    end  
+  end
+
+  def self.evolve(object)
+    case object
+      when nil then nil 
+      when Hash then object 
+      when Placing then object.mongoize
+    end
+  end
+
+end  
