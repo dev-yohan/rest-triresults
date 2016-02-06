87e4e9cd171785040ca0552c97b0b6bf2b9bca5b
yohan morales <yohanmorales@hotmail.com>
2016-02-06 11:35:33 -0500

    + spec/results_spec.rb -e rq01

---
+++ app/models/entrant.rb
@@ -0,0 +1,12 @@
+class Entrant
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  store_in collection: 'results'
+
+  field :bib,     type: Integer, as: :bib
+  field :secs,    type: Float, as: :secs
+  field :o,       type: Placing, as: :overall
+  field :gender,  type: Placing, as: :gender
+  field :group,   type: Placing, as: :group
+
+end
