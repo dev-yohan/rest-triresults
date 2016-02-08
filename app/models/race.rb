0a91a5e7775e03d7ecbed65fda05a8636505061c
yohan morales <yohanmorales@hotmail.com>
2016-02-07 20:42:17 -0500

    + spec/service_facade_spec.rb -e rq02

--- app/models/race.rb
+++ app/models/race.rb
@@ -32,42 +32,43 @@ class Race
   end
 
   DEFAULT_EVENTS.keys.each do |name|
-
     define_method("#{name}") do
       event=events.select {|event| name==event.name}.first
       event||=events.build(DEFAULT_EVENTS["#{name}"])
     end
-
       ["order","distance","units"].each do |prop|
         if DEFAULT_EVENTS["#{name}"][prop.to_sym]
-
         define_method("#{name}_#{prop}") do
           event=self.send("#{name}").send("#{prop}")
         end
-
         define_method("#{name}_#{prop}=") do |value|
           event=self.send("#{name}").send("#{prop}=", value)
         end
-
       end
-
     end
-
   end
 
 
   ["city", "state"].each do |action|
-
       define_method("#{action}") do
         self.location ? self.location.send("#{action}") : nil
       end
-
       define_method("#{action}=") do |name|
         object=self.location ||= Address.new
         object.send("#{action}=", name)
         self.location=object
       end
+  end
 
+  def get_group racer
+    if racer && racer.birth_year && racer.gender
+      quotient=(date.year-racer.birth_year)/10
+      min_age=quotient*10
+      max_age=((quotient+1)*10)-1
+      gender=racer.gender
+      name=min_age >= 60 ? "masters #{gender}" : "#{min_age} to #{max_age} (#{gender})"
+      Placing.demongoize(:name=>name)
+    end
   end
 
 end
