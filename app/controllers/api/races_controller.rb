da98308d5656cc53f21987fdd93daed5cfa5d18f
yohan morales <yohanmorales@hotmail.com>
2016-02-13 10:02:19 -0500

    + spec/uri_spec.rb -e rq03

--- app/controllers/api/races_controller.rb
+++ app/controllers/api/races_controller.rb
@@ -2,9 +2,19 @@ module Api
   class RacesController < ApplicationController
 
     def index
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races"
+      else
+        #real implementation ...
+      end
     end
     
     def show
+      if !request.accept || request.accept == "*/*"
+        render plain: "/api/races/#{params[:id]}"
+      else
+        #real implementation ...
+      end
     end  
 
   end
