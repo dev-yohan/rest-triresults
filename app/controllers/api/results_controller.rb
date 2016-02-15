77575bee9cceba3e7c9e0c4a16f56d4ecf54ffbd
yohan morales <yohanmorales@hotmail.com>
2016-02-14 22:12:47 -0500

    + spec/caching_spec.rb -e rq01

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -6,6 +6,11 @@ module Api
         render plain: "/api/races/#{params[:race_id]}/results"
       else
         #real implementation ...
+        #race.entrants.max(:updated_at)
+        @race = Race.find(params[:race_id])
+        @results = @race.entrants
+        fresh_when(last_modified: @race.entrants.max(:updated_at), public: true)
+        render :json => {success: "ok"}, :status => :ok
       end
     end
     
@@ -14,6 +19,7 @@ module Api
        render plain: "/api/races/#{params[:race_id]}/results/#{params[:id]}"
       else
         #real implementation ...
+
          @result = Race.find(params[:race_id]).entrants.where(id: params[:id]).first
          render :partial=>"result", :object=>@result
       end
