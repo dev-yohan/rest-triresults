7c2b9d9a2b08ea285ed7b5dfe1731251e3b8f955
yohan morales <yohanmorales@hotmail.com>
2016-02-14 23:14:58 -0500

    + spec/resources_spec.rb

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -1,16 +1,28 @@
 module Api
   class ResultsController < ApplicationController
 
+
+    rescue_from Mongoid::Errors::DocumentNotFound do |exception|
+      @msg = "woops: cannot find result[#{params[:id]}]"
+      if !request.accept || request.accept == "*/*"
+        render plain: @msg, status: :not_found
+      else
+        respond_to do |format|
+          format.json { render "api/error_msg", status: :not_found, content_type: "#{request.accept}" }
+          format.xml  { render "api/error_msg", status: :not_found, content_type: "#{request.accept}" }
+        end
+      end
+    end
+
+
     def index
       if !request.accept || request.accept == "*/*"
         render plain: "/api/races/#{params[:race_id]}/results"
       else
         #real implementation ...
         #race.entrants.max(:updated_at)
-        @race = Race.find(params[:race_id])
-        @results = @race.entrants
-        fresh_when(last_modified: @race.entrants.max(:updated_at), public: true)
-        render :json => {success: "ok"}, :status => :ok
+        @race=Race.find(params[:race_id])
+        @entrants=@race.entrants
       end
     end
     
@@ -22,8 +34,47 @@ module Api
 
          @result = Race.find(params[:race_id]).entrants.where(id: params[:id]).first
          render :partial=>"result", :object=>@result
+         
       end
     end  
+
+    def update
+      entrant = Race.find(params[:race_id]).entrants.where(id: params[:id]).first
+
+      #@result.update(race_params)
+      result=params[:result]
+      if result
+        if result[:swim]
+          entrant.swim=entrant.race.race.swim
+          entrant.swim_secs = result[:swim].to_f
+        end
+        if result[:t1]
+          entrant.t1=entrant.race.race.t1
+          entrant.t1_secs = result[:t1].to_f
+        end
+        if result[:t2]
+          entrant.t2=entrant.race.race.t2
+          entrant.t2_secs = result[:t2].to_f
+        end
+        if result[:bike]
+          entrant.bike=entrant.race.race.bike
+          entrant.bike_secs = result[:bike].to_f
+        end
+        if result[:run]
+          entrant.run=entrant.race.race.run
+          entrant.run_secs = result[:run].to_f
+        end
+      end  
+      entrant.save
+
+      render json: entrant, status: :ok
+
+    end 
+    
+    private
+    def result_params
+      params.require(:result).permit(:swim, :t1, :bike, :t2, :run)
+    end 
 
   end
 end
--- app/views/api/results/_result.json.jbuilder
+++ app/views/api/results/_result.json.jbuilder
@@ -1,4 +1,8 @@
-json.place result.o.place
+json.ignore_nil!
+
+if !result.o.nil?
+  json.place result.o.place
+end
 json.time format_hours(result.secs.to_i)
 json.last_name result.last_name
 json.first_name result.first_name
@@ -11,10 +15,10 @@ json.group result.group_name
 json.group_place result.group_place
 json.swim format_hours(result.swim_secs.to_i)
 json.pace_100 format_minutes(result.swim_pace_100.to_i)
-json.t1 format_hours(result.t1_secs.to_i)
+json.t1 format_minutes(result.t1_secs.to_i)
 json.bike format_hours(result.bike_secs.to_i)
 json.mph  format_mph result.bike_mph.to_f.round(1) 
-json.t2 format_hours result.t2_secs.to_i
+json.t2 format_minutes result.t2_secs.to_i
 json.run format_hours result.run_secs.to_i
 json.mmile format_minutes result.run_mmile.to_i
 json.result_url api_race_result_url( result.race.id, result)
